8f09cf454b6eb4d6f3aecc6956c400235517cb6d
João Garcia <jpmldg@gmail.com>
2016-11-11 13:55:53 -0200

    Reconhecendo area clicada

--- Batalha Naval/src/WindowFrame.java
+++ Batalha Naval/src/WindowFrame.java
@@ -1,19 +1,20 @@
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.*;  
 
 public class WindowFrame extends JFrame {
 	
-	public final int LARG_DEFAULT=800; 
+	public final int LARG_DEFAULT=940; 
 	public final int ALT_DEFAULT=600;
-	
+	private GameBoard panel = new GameBoard();
 	
 	public WindowFrame() {
 		setSize(LARG_DEFAULT,ALT_DEFAULT);
 		setDefaultCloseOperation(EXIT_ON_CLOSE); 
 		
-		
-		getContentPane().add(new GameBoard());
+		getContentPane().add(panel);
+	}
 	
+	public GameBoard getPanel(){
+		return panel;
 	}
 	
 }
